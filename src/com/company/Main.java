b9a5824de54c33ed1fd48531f2a91796585bc8ad
Marcus Wuttke <marcuswuttke@outlook.com>
2020-12-25 20:57:21 +0100

    Funkar dettta?

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a= 2;
+	String text="Svarvilicous";
+	if(a==2){
+	    System.out.println(text);
+	    a=3;
+	    String t="hej";
+    }
+    }
+}
