73e27256b57b3e0cc15779f2c929118859009f7e
Marcus Wuttke <marcuswuttke@outlook.com>
2020-12-26 04:26:38 +0100

    Commit 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,59 +6,56 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	int a= 2;
-	String text="Svarvilicous";
-	if(a==2){
-	    System.out.println(text);
-	    a=3;
-	    String t="hej";
-    }
+
 	Svarv svarv = new Svarv();
 	System.out.println("Svarv är:" + svarv.getAge());
 	System.out.println("Han bor i:" +svarv.bor(svarv.getAge()));
 
-	boolean married=false;
-
+	int svarvenTaggad=10;
 	boolean isAlive=true;
 	Scanner scan = new Scanner(System.in);
 	Random rand=new Random();
 	while(isAlive){
 		System.out.println("Vad vill du göra med Svarven?");
 		String action = scan.nextLine();
-			if (action.equals("kill")){
-				isAlive=false;
+		switch (action) {
+			case "kill":
+				isAlive = false;
 				System.out.println("Svarv is dead, all hail Svarven");
-			}
-			else if(action.equals("getAge")){
+				break;
+			case "getAge":
 				System.out.println("Svarv är:" + svarv.getAge());
-			}
-			else if(action.equals("marry")){
-				if(!svarv.checkMarried()) {
-					int chanceToMarry = rand.nextInt(5);
-					if (chanceToMarry == 3) {
+				break;
+			case "marry":
+				if (!svarv.checkMarried()) {
+					int chanceToMarry = rand.nextInt(svarvenTaggad);
+					if (chanceToMarry == 0) {
 						svarv.setMarrigeStatus(true);
 						System.out.println("Svarven sa ja :D");
 					} else {
 						System.out.println("Svarven sa nej :(");
 					}
-				}
-				else{
+				} else {
 					System.out.println("Ni är redan gifta");
 				}
-			}
-			else if(action.equals("divorce")){
-				if(svarv.checkMarried()){
+				break;
+			case "divorce":
+				if (svarv.checkMarried()) {
 					int settlement = rand.nextInt(1000000);
-					System.out.println("Du fick med: "+settlement+" kr från skiljsmässan");
+					System.out.println("Du fick med: " + settlement + " kr från skiljsmässan");
 					svarv.setMarrigeStatus(false);
-				}
-				else{
+				} else {
 					System.out.println("Ni är inte gifta");
 				}
-			}
-			else{
+				break;
+			case "flirt":
+				if(svarvenTaggad>0) {
+					svarvenTaggad--;
+				}
+			default:
 				System.out.println("Bad input");
-			}
+				break;
+		}
 
 	}
 
